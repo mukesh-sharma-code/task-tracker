5d02423b98134aa8b6cbbe71b03a3314ebdb30ba
restaboutme <mukesh.sharma.code@gmail.com>
2021-07-04 20:34:47 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <Header title="Task"/>
+    <AddTask @add-task="addTask" />
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
+  </div>  
+  
+</template>
+
+<script>
+
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+export default {
+  name: 'App',
+  components: {
+    Header,Tasks,AddTask
+  },
+  data(){
+    return{
+      tasks:[]
+    }
+  },
+  methods:{
+    deleteTask(id){
+      this.tasks = this.tasks.filter((task)=> task.id !== id)
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map((task) => 
+        task.id === id ? {...task, reminder: !task.
+        reminder } : task
+      )
+    },
+    addTask(task){
+      this.tasks = [...this.tasks,task]
+    }
+  },
+  created(){
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'March 2nd at 2:30pm',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 3rd at 3:30pm',
+        reminder: true,
+      },
+      {
+        id: 4,
+        text: 'Coding',
+        day: 'March 4th at 4:30pm',
+        reminder: true,
+      }
+    ]
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button @click="onClick()" :style={background:color}>{{ text }}</button>
+</template>
+
+<script>
+    export default {
+        name : "Button",
+        props: ['text','color'],
+        methods:{
+            onClick(){
+                console.log("button click");
+            }
+        }
+    }
+</script>
